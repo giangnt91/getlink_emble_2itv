6715382159e9d6c5f1c27d37601900ee6184637f
Trường giang <giangnt91@users.noreply.github.com>
2017-10-04 21:06:31 +0700

    add animation

--- server/controller.js
+++ server/controller.js
@@ -1,28 +1,34 @@
 var pm = angular.module('PM.controller', ['ngRoute', 'angular-clipboard'])
     .controller('PMCtrl', function ($rootScope, $scope) {
-        
+
         $scope.string;
         $scope.gen = function () {
-            console.log('a');
             if ($scope.string.indexOf("dailymotion") != -1) {
                 tmp = $scope.string.slice(-7);
                 $scope.new_link = "//www.dailymotion.com/embed/video/" + tmp;
+
+                $("#success-alert").fadeTo(2000, 1000).slideUp(500, function () {
+                    $("#success-alert").slideUp(2000);
+                });
+
                 $scope.string = null;
-            } else {
+            } else if ($scope.string.indexOf("ok.ru") != -1) {
                 tmp = $scope.string.slice(-12);
                 $scope.new_link = "//ok.ru/videoembed/" + tmp;
+
+                $("#success-alert").fadeTo(2000, 1000).slideUp(500, function () {
+                    $("#success-alert").slideUp(2000);
+                });
+
+                $scope.string = null;
+            } else {
+                $scope.error = true;
+
+                $("#danger-alert").fadeTo(2000, 1000).slideUp(500, function () {
+                    $("#danger-alert").slideUp(2000);
+                });
+
                 $scope.string = null;
             }
         };
-
-        $scope.supported = false;
-        $scope.success = function () {
-                $("#success-alert").fadeTo(2000, 500).slideUp(500, function(){
-                $("#success-alert").slideUp(2000);
-            });
-        };
-        $scope.fail = function (err) {
-            console.error('Error!', err);
-        };
-
     })
