78558517692ca4a78f7bab275a013ab738d06a9b
Trường giang <giangnt91@users.noreply.github.com>
2017-10-04 20:39:12 +0700

    update null text box

--- server/controller.js
+++ server/controller.js
@@ -1,13 +1,17 @@
 var pm = angular.module('PM.controller', ['ngRoute', 'angular-clipboard'])
     .controller('PMCtrl', function ($rootScope, $scope) {
         
-        $scope.gen = function (string) {
-            if (string.indexOf("dailymotion") != -1) {
-                tmp = string.slice(-7);
+        $scope.string;
+        $scope.gen = function () {
+            console.log('a');
+            if ($scope.string.indexOf("dailymotion") != -1) {
+                tmp = $scope.string.slice(-7);
                 $scope.new_link = "//www.dailymotion.com/embed/video/" + tmp;
+                $scope.string = null;
             } else {
-                tmp = string.slice(-12);
+                tmp = $scope.string.slice(-12);
                 $scope.new_link = "//ok.ru/videoembed/" + tmp;
+                $scope.string = null;
             }
         };
 
